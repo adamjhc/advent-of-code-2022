d9e023287b132cf8087c24e9c3d9d48776025d2b
Adam Cox <adamjhcox@gmail.com>
2022-12-02 15:13:40 +0000

    Day 02 Part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     println!("{}", part_one());
+    println!("{}", part_two());
 }
 
 fn part_one() -> usize {
@@ -31,6 +32,39 @@ fn part_one() -> usize {
             };
 
             score_shape + score_outcome
+        })
+        .sum()
+}
+
+fn part_two() -> usize {
+    utils::read_string("./input.txt")
+        .lines()
+        .map(|round| {
+            let mut turns = round.split_ascii_whitespace();
+            let turn_opponent = turns.next().unwrap();
+            let turn_mine = turns.next().unwrap();
+
+            let score_outcome = match turn_mine {
+                "X" => 0,
+                "Y" => 3,
+                "Z" => 6,
+                _ => panic!(),
+            };
+
+            let score_shape = match (turn_opponent, turn_mine) {
+                ("A", "X") => 3,
+                ("B", "X") => 1,
+                ("C", "X") => 2,
+                ("A", "Y") => 1,
+                ("B", "Y") => 2,
+                ("C", "Y") => 3,
+                ("A", "Z") => 2,
+                ("B", "Z") => 3,
+                ("C", "Z") => 1,
+                _ => panic!(),
+            };
+
+            score_outcome + score_shape
         })
         .sum()
 }
