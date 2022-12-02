204320ed10180280114be83e37ef18215783fd08
Adam Cox <adamjhcox@gmail.com>
2022-12-02 10:09:06 +0000

    Day 01 Part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,8 +1,20 @@
 fn main() {
     println!("{}", part_one());
+    println!("{}", part_two());
 }
 
 fn part_one() -> usize {
+    get_elves_calorie_sums().into_iter().max().unwrap()
+}
+
+fn part_two() -> usize {
+    let mut calories = get_elves_calorie_sums();
+    calories.sort_unstable();
+    let len = calories.len();
+    calories[len - 1] + calories[len - 2] + calories[len - 3]
+}
+
+fn get_elves_calorie_sums() -> Vec<usize> {
     utils::read_string("./input.txt")
         .split("\n\n")
         .map(|elf_calories| {
@@ -11,6 +23,5 @@ fn part_one() -> usize {
                 .map(|calorie| calorie.parse::<usize>().unwrap())
                 .sum()
         })
-        .max()
-        .unwrap()
+        .collect()
 }
