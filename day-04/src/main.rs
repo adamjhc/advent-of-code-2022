4ff549f32059ece31afd9ff2e4959209552efda5
Adam Cox <adamjhcox@gmail.com>
2022-12-04 17:32:20 +0000

    Day 04 Part 1

---
+++ day-04/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    println!("{}", part_one());
+}
+
+fn part_one() -> usize {
+    include_str!("../input.txt")
+        .lines()
+        .filter(|line| {
+            let mut split = line.split(',');
+            let range_one = parse_range(split.next().unwrap());
+            let range_two = parse_range(split.next().unwrap());
+
+            (range_one.0 <= range_two.0 && range_two.1 <= range_one.1)
+                || (range_two.0 <= range_one.0 && range_one.1 <= range_two.1)
+        })
+        .count()
+}
+
+fn parse_range(range: &str) -> (usize, usize) {
+    let mut split = range.split('-');
+    (
+        split.next().unwrap().parse::<usize>().unwrap(),
+        split.next().unwrap().parse::<usize>().unwrap(),
+    )
+}
