bb691c30f1face5e0209a24d5edceaa1ca49cae7
Adam Cox <adamjhcox@gmail.com>
2022-12-06 07:39:57 +0000

    Day 06 Part 2

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 
 fn main() {
     println!("{}", part_one());
+    println!("{}", part_two());
 }
 
 fn part_one() -> usize {
@@ -11,4 +12,13 @@ fn part_one() -> usize {
         .position(|window| HashSet::<&char>::from_iter(window).len() == 4)
         .unwrap()
         + 4
+}
+
+fn part_two() -> usize {
+    let chars: Vec<char> = include_str!("../input.txt").chars().collect();
+    chars
+        .windows(14)
+        .position(|window| HashSet::<&char>::from_iter(window).len() == 14)
+        .unwrap()
+        + 14
 }
